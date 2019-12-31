dd20aa8f53f33ab26e00a4f713b4a786f654321b
Hari Suminto <sumintohpw@gmail.com>
2019-12-31 20:36:42 +1000

    Injected Vuetify

---
+++ content/AspNetCore.VueJs/VueApp/idontneedtypescript/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+
+
+Vue.use(Vuex)
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+});
